55ce53b99c303c4d74e3ac2608940a5a59fd969d
TravisC17 <tjcredillejr@gmail.com>
2026-02-03 07:36:57 -0500

    Update MiniGameFinishBlock.cs

--- VarmintMadness/Assets/Scripts/MiniGameFinishBlock.cs
+++ VarmintMadness/Assets/Scripts/MiniGameFinishBlock.cs
@@ -4,21 +4,30 @@ public class MiniGameFinishBlock : MonoBehaviour
 {
     public MiniGameMusic musicPlayer; // Drag MusicManager here in Inspector
 
+    private bool hasPlayedFinishMusic = false; // NEW FLAG
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("FinishBlock triggered by: " + collision.name);
 
-        // If you want tag check, make sure marbles are tagged "Marble"
         if (collision.CompareTag("Marble"))
         {
-            if (musicPlayer != null)
+            if (!hasPlayedFinishMusic) // Only play once
             {
-                Debug.Log("Playing finish song!");
-                musicPlayer.PlayFinishSong();
+                if (musicPlayer != null)
+                {
+                    Debug.Log("Playing finish song!");
+                    musicPlayer.PlayFinishSong();
+                    hasPlayedFinishMusic = true; // Prevent future plays
+                }
+                else
+                {
+                    Debug.LogWarning("MusicPlayer reference is missing!");
+                }
             }
             else
             {
-                Debug.LogWarning("MusicPlayer reference is missing!");
+                Debug.Log("Finish music already played, ignoring.");
             }
         }
     }
